2e2fb8d0c1394bae7439e502822e075e4b5699bb
dwivedialind <84173548+dwivedialind@users.noreply.github.com>
2024-02-09 11:30:24 +0530

    BubbleSort Algorithm

---
+++ Introduction/src/SimpleSorting/bubbleSort.java
@@ -0,0 +1,63 @@
+package SimpleSorting;
+
+class ArrayBub{
+    private long[] a;
+    private int nElems;
+
+    public ArrayBub(int size){
+        a = new long[size];
+        nElems = 0;
+    }
+
+    public void insert(long value){
+        a[nElems] = value;
+        nElems++;
+    }
+
+    public void display(){
+        for(int i = 0 ; i < nElems ; i++)
+            System.out.print(a[i]+ " ");
+        System.out.println("");
+    }
+
+    public void bubbleSort(){
+        for(int i = 0 ; i < nElems; i++){
+            for(int j = 0; j < nElems-1-i; j++ )
+                if(a[j] > a[j+1])
+                    swap(j);
+        }
+    }
+
+    private void swap(int j){
+        long temp = a[j];
+        a[j] = a[j+1];
+        a[j+1] = temp;
+    }
+}
+
+public class bubbleSort {
+
+    public static void main(String[] args)
+    {
+        int maxSize = 100;            // array size
+        ArrayBub arr;                 // reference to array
+        arr = new ArrayBub(maxSize);  // create the array
+
+        arr.insert(77);               // insert 10 items
+        arr.insert(99);
+        arr.insert(44);
+        arr.insert(55);
+        arr.insert(22);
+        arr.insert(88);
+        arr.insert(11);
+        arr.insert(00);
+        arr.insert(66);
+        arr.insert(33);
+
+        arr.display();                // display items
+
+        arr.bubbleSort();             // bubble sort them
+
+        arr.display();                // display them again
+    }  // end main(
+}

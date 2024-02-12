4e0940b1312184604942321bf38f8b16b9dd278b
dwivedialind <84173548+dwivedialind@users.noreply.github.com>
2024-02-12 09:41:18 +0530

    Simple Linked List

---
+++ Introduction/src/LinkedList/LinkListApp.java
@@ -0,0 +1,72 @@
+package LinkedList;
+class Node{
+    public int data; // int data
+
+    public Node next; // reference to next node
+    public Node(int d){
+        data = d;
+      // next is automatically set to null
+    }
+    public void displayNode(){ // display a node.
+        System.out.println("{ "+ data+" }");
+    }
+}
+
+class LinkList{
+    private Node first; // reference to first node
+    public LinkList(){
+        first = null; //this is to make it clear however automatically it set to null only.
+    }
+
+    public boolean isEmpty(){
+        return (first == null);
+    }
+    public void insertFirst(int d){ // insert at start of list
+        Node newNode = new Node(d); //create a new nod
+        newNode.next = first; //next of new node points to old fist (newNode --> oldfirst)
+        first = newNode;// first now point to new Node
+    }
+
+    public Node deleteFirst(){//deletes first node
+        if(isEmpty())
+            return null;
+        Node temp = first; // hold first node in temp variable
+        first = first.next; //first-->oldfirst.next
+        return temp; // return deleted Node
+    }
+
+    public void displayList(){
+        if(isEmpty()) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node current = first;
+        while(current!=null){
+            current.displayNode(); // display current node
+            current = current .next; // moves to next node
+        }
+        System.out.println("");
+    }
+
+
+
+
+}
+public class LinkListApp {
+    public static void main(String[] args) {
+        LinkList theList = new LinkList(); // make new list
+        theList.insertFirst(22); // insert four items
+        theList.insertFirst(44);
+        theList.insertFirst(66);
+        theList.insertFirst(88);
+        theList.displayList(); // display list
+        while( !theList.isEmpty() ) // until it’s empty,
+        {
+            Node aLink = theList.deleteFirst(); // delete link
+            System.out.print("Deleted"); // display it
+            aLink.displayNode();
+            System.out.println("");
+        }
+        theList.displayList(); // display list
+    }
+}
